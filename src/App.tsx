c756f4b3ead20114222fadc18002f483c85a882d
mttaskin <0mttaskin1@gmail.com>
2023-04-08 11:37:26 +0300

    v1

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import FavoritesPage from "./pages/FavoritesPage";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="bg-slate-500 min-h-screen text-center">
-      <p className="text-white p-20">Hello Typescript + TailwindCss</p>
+      <BrowserRouter>
+        <NavBar/>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/favorites" element={<FavoritesPage/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
