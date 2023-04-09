25211dbce8098566ca3bbc578c875cfc9e5ed824
mttaskin <0mttaskin1@gmail.com>
2023-04-09 09:26:47 +0300

    v7

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,77 +1,80 @@
-import React, { useEffect, useState } from 'react'
-import SearchComp from '../components/SearchComp'
-import axios from 'axios';
-import { useAppDispatch, useAppSelector } from '../app/hooks';
-import { fetchFail, fetchStart, getSuccessProduct } from '../features/productsSlice';
-import { EventFunc, Products } from '../models/models';
-
+import React, { useEffect, useState } from "react";
+import SearchComp from "../components/SearchComp";
+import axios from "axios";
+import { useAppDispatch, useAppSelector } from "../app/hooks";
+import {
+  addFavorites,
+  fetchFail,
+  fetchStart,
+  getSuccessProduct,
+} from "../features/productsSlice";
+import { VoidFunc } from "../models/models";
+import { EventFunc, Product, Products } from "../models/models";
+import Card from "../components/Card";
+import { toastSuccessNotify, toastWarnNotify } from "../helper/ToastNotify";
 
 const Home = () => {
-  const [search,setSearch] = useState<string>("")
+  const [search, setSearch] = useState<string>("");
   const dispatch = useAppDispatch();
-  const { loading, error, productsList } = useAppSelector(state => state.products);
+  const { loading, error, productsList,favorites } = useAppSelector(
+    (state) => state.products
+  );
 
+  
   const getData = async () => {
-    dispatch(fetchStart())
+    dispatch(fetchStart());
     try {
+ 
       const { data } = await axios.get<Products>(
-        `https://dummyjson.com/prodsucts/search?q=${search}`
+        `https://dummyjson.com/products/search?q=${search}`
       );
-      console.log(data.products)
+      console.log(data.products);
       dispatch(getSuccessProduct(data.products));
     } catch (error) {
-      console.log(error)
-      dispatch(fetchFail())
+      console.log(error);
+      dispatch(fetchFail());
+    }
+  };
+  console.log(error);
+  useEffect(() => {
+    getData();
+  }, [search]);
+
+
+  const handleChange: EventFunc = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const handleAdd = (product:Product) => {
+    if(favorites.filter(item => item.id === product.id).length ===0){
+      dispatch(addFavorites(product))
+      toastSuccessNotify("Product added favorites!")
+    }else {
+      toastWarnNotify("Already added to favorites!")
     }
   }
-console.log(error)
-  useEffect(() => {
-    getData()
-  }, [search])
-  
-  // const handleChange = (e:React.ChangeEvent<HTMLInputElement>) => {
-  //   setSearch(e.target.value)
-  // }
-  const handleChange:EventFunc = (e) => {
-    setSearch(e.target.value);
-  }
+
 
   return (
     <div>
       <SearchComp handleChange={handleChange} />
-      {loading ? error ? (
-          <div>
-            <p className="text-center text-red-600">Something went wrong...</p>
-          </div>
-        ) : (
-          <div>
-            <p className="text-center text-red-600">Products loading...</p>
-          </div>
-        )
-      : (
-        <div>
-          {productsList.map((item) => (
-            <p>{item.title}</p>
-          ))}
-        </div>
-      )}
-      {/* {loading ? (
-        <div>
+      {loading ? (
+        <div className="mt-52">
           <p className="text-center text-red-600">Products loading...</p>
         </div>
       ) : error ? (
-        <div>
+        <div className="mt-52">
           <p className="text-center text-red-600">Something went wrong...</p>
         </div>
       ) : (
-        <div>
-          {productsList.map((item) => (
-            <p>{item.title}</p>
-          ))}
+        <div className="flex justify-center items-center flex-wrap gap-5 p-5">
+          {productsList.map((item) => <Card key={item.id} text="Add to favorites" item={item} handleFunc={handleAdd} />)}
         </div>
-      )} */}
+      )}
     </div>
   );
-}
+};
 
-export default Home
+export default Home;
+
+
