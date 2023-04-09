7bada873a777202f4791645466d13fe13cd24931
mttaskin <0mttaskin1@gmail.com>
2023-04-09 08:14:35 +0300

    v5

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const NavBar = () => {
   return (
     <div className='flex justify-between items-center px-5 h-[50px] text-white bg-gray-500'>
-        <h3 className='font-bold italic'>Clarus Store</h3>
+        <h3 className='font-bold italic'>MTT Store</h3>
         <div>
             <Link to="/" className='mr-2' >Home</Link>
             <Link to="/favorites" >Favorites</Link>
